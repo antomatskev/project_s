2f997926afda83cb9e3a1391b56d8aac9ad29266
Anton-7 <antmat@ttu.ee>
2021-05-28 10:33:26 +0300

    initial commit

---
+++ src/main/java/com/example/project_s/ProjectSApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_s/ProjectSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
