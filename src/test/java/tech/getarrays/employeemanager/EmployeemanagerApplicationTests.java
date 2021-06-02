47ef385938d16cf2cb393812e1d345cae01125ac
TelJek <50630711+TelJek@users.noreply.github.com>
2021-06-02 21:17:33 +0300

    Example project Backend

--- src/test/java/tech/getarrays/employeemanager/EmployeemanagerApplicationTests.java
+++ src/test/java/tech/getarrays/employeemanager/EmployeemanagerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.project_s;
+package tech.getarrays.employeemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectSApplicationTests {
+class EmployeemanagerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
