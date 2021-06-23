35d4f3f9b96da11372633475292ea6ac89375923
TelJek <50630711+TelJek@users.noreply.github.com>
2021-06-23 15:52:16 +0300

    Mentor Page v0.1 #test mapping removed

--- backend/src/main/java/eu/mentreon/backend/MentorPageResource.java
+++ backend/src/main/java/eu/mentreon/backend/MentorPageResource.java
@@ -1,8 +1,5 @@
 package eu.mentreon.backend;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mentor")
 public class MentorPageResource {
 
-    @GetMapping("/test")
-    public ResponseEntity<String> testController () {
-        return new ResponseEntity<>("testController respond", HttpStatus.OK);
-    }
 }
