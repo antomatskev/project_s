be079239c9dbf06f5e3645919a3ac95010fb6f1e
TelJek <50630711+TelJek@users.noreply.github.com>
2021-06-24 14:02:49 +0300

    Update MentorPageResource.java

--- backend/src/main/java/eu/mentreon/backend/MentorPageResource.java
+++ backend/src/main/java/eu/mentreon/backend/MentorPageResource.java
@@ -5,6 +5,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mentor")
-public class MentorPageResource {
-
-}
+public class MentorPageResource {}
